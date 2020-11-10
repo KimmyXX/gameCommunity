bd7b8053b6d5286ef230156ed6865d1d5820fd8c
KimmyXX <2922093951@qq.com>
2020-11-10 23:51:41 +0800

    新增管理员模块完成

--- src/adminComponents/addAdmin.vue
+++ src/adminComponents/addAdmin.vue
@@ -1,43 +1,140 @@
 <template>
-  <div>
-      <!-- <el-page-header content="新增管理员"></el-page-header>
-      <el-form ref="form" :model="form" label-width="5rem">
-          <el-form-item label="账号">
-              <el-input type="text" v-model="form.adminId"></el-input>
-          </el-form-item>
-          <el-form-item label="密码">
-              <el-input type="text" v-model="form.adminPassword"></el-input>
-          </el-form-item>
-          <el-form-item label="确认密码">
-              <el-input type="text" v-model="form.confirmAdminPassword"></el-input>
-          </el-form-item>
-          <el-form-item>
-              <el-button type="primary">确认新增管理员</el-button>
-          </el-form-item>
-      </el-form> -->
-      <!-- <el-table :data="adminArr">
-          <el-table-column></el-table-column>
-      </el-table> -->
+  <div class="addAdminOut">
+    <h2>新增管理员</h2>
+    <el-form ref="form" :model="form" :rules="rules" label-width="5rem">
+      <el-form-item label="账号" prop="adminId">
+        <el-input type="text" v-model="form.adminId"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="adminPassword">
+        <el-input type="password" v-model="form.adminPassword"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="cofirmAdminPassword">
+        <el-input type="password" v-model="form.cofirmAdminPassword"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="addAdmin">确认新增管理员</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            form: {
-                adminId: "",
-                adminPassword: "",
-                confirmAdminPassword: ""
-            },
-            adminArr: []
+  data() {
+    let vm = this;
+    // 校验管理员账号
+    let checkAdminId = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error("账号不能为空"));
+      } else {
+        let reg = /^[0-9]{10}$/;
+        if (!reg.test(value)) {
+          callback(new Error("请输入10位数字组成的密码"));
+        } else {
+          callback();
         }
-    }
-}
+      }
+    };
+    // 校验密码
+    let checkAdminPassword = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error("密码不能为空"));
+      } else {
+        let reg = /^[a-zA-Z0-9]+$/;
+        if (!reg.test(value)) {
+          callback(new Error("请输入数字和英文组成的密码"));
+        } else {
+          callback();
+        }
+      }
+    };
+    // 校验确认密码
+    let checkConfirmAdminPassword = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error("确认密码不能为空"));
+      } else {
+        if (vm.form.cofirmAdminPassword != vm.form.adminPassword) {
+          callback(new Error("确认密码必须与密码一致"));
+        } else {
+          callback();
+        }
+      }
+    };
+    return {
+      form: {
+        adminId: "",
+        adminPassword: "",
+        cofirmAdminPassword: "",
+      },
+      rules: {
+        adminId: [
+          {
+            validator: checkAdminId,
+            trigger: "blur",
+          },
+        ],
+        adminPassword: [
+          {
+            validator: checkAdminPassword,
+            trigger: "blur",
+          },
+        ],
+        cofirmAdminPassword: [
+          {
+            validator: checkConfirmAdminPassword,
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    addAdmin() {
+      this.$refs.form.validate((passFlag, notPassField) => {
+        if (passFlag) {
+          this.$http
+            .post("addAdmin", {
+              adminId: this.form.adminId,
+              adminPassword: this.form.adminPassword,
+            })
+            .then(({ data }) => {
+              if (data.success) {
+                  this.$message({
+                      type: "success",
+                      message: "新增管理员成功"
+                  })
+                  this.$refs.form.resetFields();
+              } else {
+                this.$message({
+                  type: "warning",
+                  message: "账号重复",
+                });
+              }
+            })
+            .catch((err) => {
+              this.$message.error("连接服务器错误");
+            });
+        } else {
+          this.$message({
+            type: "warning",
+            message: "注册信息错误",
+          });
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.el-form {
+.addAdminOut {
+  width: 100%;
+  padding: 30px;
+  h2 {
+    margin-bottom: 20px;
+  }
+  .el-form {
     width: 50%;
+  }
 }
 </style>
